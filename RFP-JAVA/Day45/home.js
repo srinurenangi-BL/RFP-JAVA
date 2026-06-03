5e04b29e44b740515b7d32bacb87e509ba12c20c
srinurenangi-BL <srinu.renangi@bridgelabz.com>
2026-06-03 21:44:04 +0530

    Code done Day45

---
+++ RFP-JAVA/Day45/home.js
@@ -0,0 +1,43 @@
+let empPayrollList;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    createInnerHtml();
+});
+
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? 
+           JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+};
+
+const createInnerHtml = () => {
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
+    if (empPayrollList.length == 0) return;
+    
+    let innerHtml = `${headerHtml}`;
+    for (const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
+        <tr>
+            <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td>${getDeptHtml(empPayrollData._department)}</td>
+            <td>${empPayrollData._salary}</td>
+            <td>${empPayrollData._startDate}</td>
+            <td>
+                <img id="${empPayrollData._id}" onclick="remove(this)" src="assets/delete-black-18dp.svg" alt="delete">
+                <img id="${empPayrollData._id}" onclick="update(this)" src="assets/create-black-18dp.svg" alt="edit">
+            </td>
+        </tr>`;
+    }
+    document.querySelector('#table-display').innerHTML = innerHtml;
+};
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`;
+    }
+    return deptHtml;
+};
